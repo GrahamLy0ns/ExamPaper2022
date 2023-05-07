4470d91edcb5dae404dc8a67cc289566e9c1d1fe
GrahamLy0ns <s00225961@atu.ie>
2023-05-07 17:37:43 +0100

    Add project files.

---
+++ ExamPaper2022/RentalProperty.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamPaper2022
+{
+    public enum RentalType
+    {
+        House,
+        Flat,
+        Share
+    }
+    public class RentalProperty
+    {
+        public int ID { get; set; }
+        public RentalType RentalType { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public void IncreaseRentByPerchentageAmount(decimal amount, decimal Price)
+        {
+            Price *= amount;
+        }
+
+        
+        
+    }
+
+    public class PropertyData : DbContext
+    {
+        public PropertyData() : base("PropertyData") { }
+        public DbSet<RentalProperty> Properties { get; set; }
+    }
+}
